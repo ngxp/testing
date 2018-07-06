c06dfa569f57e206357ac53f2604b81ebbd861ab
joostme <github@joost.io>
2018-07-06 16:53:31 +0200

    feat: configure jest and jasmine in example application

--- src/app/app.component.spec.jest.ts
+++ src/app/app.component.spec.jest.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture } from '@angular/core/testing';
 import { createComponent, setupComponentTest } from '@ngx-patterns/testing';
+import { expectElementFromFixture } from '@ngx-patterns/testing-jasmine';
 import { AppComponent } from './app.component';
 describe('AppComponent', () => {
     let fixture: ComponentFixture<AppComponent>;
@@ -15,10 +16,7 @@ describe('AppComponent', () => {
         fixture.detectChanges();
     });
 
-    it('should create the app', async(() => {
-        expect(fixture).toBeTruthy();
-    }));
-    it(`should have as title 'tst'`, async(() => {
-        expect(fixture.componentInstance.title).toEqual('tst');
+    it(`should have a h1`, async(() => {
+        expectElementFromFixture(fixture, 'h1').toBeTruthy();
     }));
 });
